83a657f1b23c0d9506c3041ec59ed4e05d3028ef
pjudge65 <pjudge65@gmail.com>
2021-08-04 10:55:18 -0500

    Updated project with optional requirements - random colorizer and 10% black shader

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
 let n = 16;
 
 
+
 const container = document.querySelector(".container");
 populate(n);
 
@@ -37,16 +38,103 @@ container.style.gridTemplateRows = `repeat(${n}, auto)`;
 const nodes = document.querySelectorAll(".testDiv");
 nodes.forEach((box) => {
     box.addEventListener("mouseover", () => {
-        box.classList.add('onHover')
+        if (processColor(box)[0]==255 && processColor(box)[1]==255 && processColor(box)[2]==255) {
+            randomColor(box)
+            //add10PercBlack(box)
+            //randomColor(box);
+            console.log(processColor(box));
+        }
+        else {
+            add10PercBlack(box)
+        }
+        //box.classList.add('onHover')
     });
 });
 }
 
 
 
+//the challenge is that red blue and green are created as global variables and singular variables
+//but we need to be access the red, blue and green values for each box
 
-//for user input
-//Clear Container div first
-//create a for loop 1 to n creating nxn divs and appending them to the container parent div
+function randomColor(box) {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    box.style.backgroundColor = `rgb(${red}, 
+                                      ${green}, 
+                                        ${blue})`
+  }
 
-//access the grid-template-columns and grid-template-rows and set them to repeat(n, autoß)
+
+  function add10PercBlack(box) {
+
+    let red = processColor(box)[0];
+    let green = processColor(box)[1];
+    let blue = processColor(box)[2];
+    
+    if (red-25.5 < 0){
+      red=0;
+    }
+    else {
+      red = red-25.5;
+    }
+
+    if (green-25.5 < 0) {
+      green=0;
+    }
+    else {
+      green = green-25.5;
+    }
+
+    if (blue-25.5 <0) {
+      blue =0;
+    }
+    else {
+      blue = blue-25.5;
+    }
+
+    box.style.backgroundColor = `rgb(${red}, 
+                                      ${green}, 
+                                        ${blue})`
+
+
+  }
+
+
+
+  function processColor(box) {
+    const style = window.getComputedStyle(box);
+    let mainString;
+    let rgbArray;
+    let backColor = style.getPropertyValue("background-color");
+    if (backColor.split("(")[0].length == 4) {
+      mainString = backColor.slice(5, -1);
+      rgbArray = mainString.split(", ");
+    }
+    else {
+      mainString = backColor.slice(4, -1);
+      rgbArray = mainString.split(", ");
+    }
+    return rgbArray;
+  }
+
+  /*function processColor(box) {
+
+
+
+    const style = window.getComputedStyle(box);
+    let backColor = style.getPropertyValue("background-color");
+    //let mainString = backColor.slice(4, -1);
+    //rgbArray = mainString.split(", ");
+    //return rgbArray;
+    return backColor;
+  }*/
+
+
+
+
+
+
+//for optional requirements
+//
